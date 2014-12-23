06255e045494a0454655360472f01dd92865969f
leekokkuen <84341838@qq.com>
2014-12-23 17:17:41 +0800

    过滤器改造
    过滤器改造

--- src/com/bone/web/ViewSpaceFilter.java
+++ src/com/bone/web/ViewSpaceFilter.java
@@ -9,8 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.bone.domain.user.User;
+
 import org.apache.commons.lang.StringUtils;
 
 
@@ -42,6 +44,7 @@ public class ViewSpaceFilter implements Filter {
 			// ②-2 设置过滤标识，防止一次请求多次过滤
 			request.setAttribute(FILTERED_REQUEST, Boolean.TRUE);
 			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpServletResponse httpResponse = (HttpServletResponse)response;
 			User userContext = getSessionUser(httpRequest);
 
 			// ②-3 用户未登录, 且当前URI资源需要登录才能访问
@@ -56,8 +59,7 @@ public class ViewSpaceFilter implements Filter {
 				//httpRequest.getSession().setAttribute(LOGIN_TO_URL, toUrl);
 
 				// ②-5转发到登录页面
-				request.getRequestDispatcher("/basic.html").forward(request,
-						response);
+				httpResponse.sendRedirect(getPortalHostAddress(httpRequest)+"/bone/basic.html");
 				return;
 			}
 			chain.doFilter(request, response);
@@ -85,6 +87,15 @@ public class ViewSpaceFilter implements Filter {
 	protected User getSessionUser(HttpServletRequest request) {
 		return (User) request.getSession().getAttribute(USER_CONTEXT);
 	}
+	
+	private  String getPortalHostAddress(HttpServletRequest request){
+    	//如果使用apache反向代理来分发请求从"X-FORWARDED-HOST"头取port主机地址
+        String portalHost =  request.getHeader("X-FORWARDED-HOST");
+        if(StringUtils.isBlank(portalHost)){//使用其他方式，如前后端部署在一个中间件上或用JK,AJP代理的方式apache与应用服务器连接
+        	portalHost = request.getHeader("HOST");
+        }
+        return request.getScheme() + "://" + portalHost;
+    }
 
 	public void destroy() {
 
